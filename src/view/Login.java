13e85463509380f2380beb810bf4a187f9930161
fellipecoel <fellipe.2401@gmail.com>
2024-02-05 11:50:49 -0300

    Criação login1

--- src/view/Login.java
+++ src/view/Login.java
@@ -2,10 +2,14 @@ package view;
 
 import javax.swing.JDialog;
 import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.Connection;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+import model.DAO;
 
 import java.awt.EventQueue;
 import java.awt.Font;
@@ -20,16 +24,21 @@ public class Login extends JDialog {
 	private JTextField inputLogin;
 	private JPasswordField inputSenha;
 	private JLabel imgDatabase;
-	
-	
+
 	public Login() {
-		addwindowListerner(new windowAdapter() {
+
+		addWindowListener(new WindowAdapter() {
 			public void windowActivated(WindowEvent e) {
 				statusConexaoBanco();
 			}
 		});
-		}
-	
+
+		setTitle("Login");
+		setResizable(false);
+		setBounds(new Rectangle(0, 0, 441, 305));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/img/logo.png")));
+		getContentPane().setLayout(null);
+
 		getContentPane().setBackground(new Color(255, 255, 255));
 		setTitle("Login");
 		setResizable(false);
@@ -37,61 +46,64 @@ public class Login extends JDialog {
 		getContentPane().setFont(new Font("Tahoma", Font.PLAIN, 15));
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/img/logo.png")));
 		getContentPane().setLayout(null);
-		
+
 		JLabel txtLogin = new JLabel("Login");
 		txtLogin.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		txtLogin.setBounds(265, 57, 87, 20);
 		getContentPane().add(txtLogin);
-		
+
 		JLabel txtSenha = new JLabel("Senha");
 		txtSenha.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtSenha.setBounds(262, 150, 80, 32);
 		getContentPane().add(txtSenha);
-		
+
 		inputLogin = new JTextField();
 		inputLogin.setBounds(180, 88, 206, 20);
 		getContentPane().add(inputLogin);
 		inputLogin.setColumns(10);
-		
+
 		inputSenha = new JPasswordField();
 		inputSenha.setBounds(180, 193, 206, 20);
 		getContentPane().add(inputSenha);
-		
+
 		JButton btnLogin = new JButton("Entrar");
 		btnLogin.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnLogin.setBounds(228, 284, 114, 23);
 		getContentPane().add(btnLogin);
-		
+
 		JLabel tituloLogin = new JLabel("Acessar conta");
 		tituloLogin.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		tituloLogin.setBounds(228, 11, 221, 20);
 		getContentPane().add(tituloLogin);
-		
-		JLabel imgDatabase = new JLabel("");
+
+		imgDatabase = new JLabel("");
 		imgDatabase.setIcon(new ImageIcon(Login.class.getResource("/img/databaseOff.png")));
 		imgDatabase.setBounds(10, 265, 46, 59);
 		getContentPane().add(imgDatabase);
 	}
 
 	DAO dao = new DAO();
-	
+
 	private void statusConexaoBanco() {
 		try {
-			Connection conexaoBannco = dao.conectar();
-			
+			Connection conexaoBanco = dao.conectar();
+
 			if (conexaoBanco == null) {
-				//Escolher a imagem
-				imgDatabase.setIcon(new ImageIcon (Login.class.getResource("/img/dataBaseOff.png"))); 
+				// Escolher a imagem
+				imgDatabase.setIcon(new ImageIcon(Login.class.getResource("/img/dataBaseOff.png")));
+			} else {
+				imgDatabase.setIcon(new ImageIcon(Login.class.getResource("/img/dataBaseOn.png")));
 			}
-			else {
-				imgDatabase.setIcon(new ImageIcon (Login.class.getResource("/img/dataBaseOn.png")));
-			}
-		
+
+			conexaoBanco.close();
+		}
+
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
 	}
-	
-	
-	
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -99,14 +111,13 @@ public class Login extends JDialog {
 					Login dialog = new Login();
 					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 					dialog.setVisible(true);
-				
-				}
-				catch (Exception e) {
+
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
-				
+
 			}
-		
+
 		});
 
 	}
